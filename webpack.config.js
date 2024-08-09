e41a2c4c0f17079724b3e979c0de666e49948304
Igor Vazhenin <vidok0577@gmail.com>
2024-08-09 09:27:44 +0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCSSExtractPlugin.loader,
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new MiniCSSExtractPlugin()
+    ]
+}
